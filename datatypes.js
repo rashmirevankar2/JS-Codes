9a27f2a1b3484876c6bfa02509c70dd771f31158
rashmirevankar2 <rashmirevankar2@gmail.com>
2025-01-12 02:58:35 +0530

    added 1 file

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+//number
+var a=90;
+let price=99.9;
+console.log(a);
+console.log(price);
+
+//string
+var str='kod'
+console.log(str, typeof str);
+
+//boolean
+let c=true;
+var d= false;
+console.log(c);
+console.log(d, typeof d);
+
+//undefined
+let z;
+console.log(z);
+
+//null
+let e=null;
+console.log(e);
